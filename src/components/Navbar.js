43f694dad1c8390b0f98cfa69f461c8a166cbcdc
Advay Anand <advayanand37@gmail.com>
2025-08-05 23:15:49 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/Advay.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -120,4 +119,5 @@ function NavBar() {
 }
 
 export default NavBar;
+
 
